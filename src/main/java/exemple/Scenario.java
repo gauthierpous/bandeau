cda92e8ffa6836da1df667a3a8d3afd02d01a304
Gauthier <gauthierpous@bbox.fr>
2020-12-08 09:22:27 +0100

    Implémentation classe Scenario

---
+++ src/main/java/exemple/Scenario.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exemple;
+
+import bandeau.Bandeau;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author gauthierpous
+ */
+public class Scenario {
+    
+    public Bandeau b;
+    private ArrayList<Effet> lesEffets = new ArrayList<>();
+
+    public Scenario(Bandeau b) {
+        this.b = b;
+    }
+    
+    public void addEffets(Effet effet){
+        this.lesEffets.add(effet);
+    }
+    
+    public void afficheScenario(){
+        lesEffets.forEach((e) -> {
+            e.afficher();
+            b.sleep(1000);
+        });
+        b.close();
+    }
+}
